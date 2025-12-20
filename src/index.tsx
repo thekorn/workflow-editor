7929712d77771048f35f8e0157eaa1e25496a2fb
Markus Korn <markus.korn@gmail.com>
2025-12-20 11:26:36 +0100

    Initialize project with Solid.js and Vite setup
    - Implement initial setup of a Solid.js project with Tailwind CSS using Vite
    - Configure development environment with `.gitignore`, TypeScript, and tooling for consistent formatting and code actions
    - Establish project structure with essential configurations and scripts for build, development, and deployment

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+/* @refresh reload */
+import './index.css';
+import { render } from 'solid-js/web';
+import 'solid-devtools';
+
+import WorkflowEditor from './WorkflowEditor';
+
+const root = document.getElementById('root');
+
+if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
+  throw new Error(
+    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+  );
+}
+
+// biome-ignore lint/style/noNonNullAssertion: we know its there
+render(() => <WorkflowEditor />, root!);
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import tailwindcss from '@tailwindcss/vite';
+import devtools from 'solid-devtools/vite';
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+
+export default defineConfig({
+  plugins: [devtools(), solidPlugin(), tailwindcss()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
+});
